ef2f0a98945fc45afc938b52198d6f6f0e0ebdc3
Teal Wolf 25 <teal@tw25.net>
2026-04-10 20:54:58 -0400

    template

---
+++ src/client/java/dev/evokerking/elemancy/client/ElemancyClient.java
@@ -0,0 +1,10 @@
+package dev.evokerking.elemancy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElemancyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
