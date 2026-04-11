8f8427f657a6e17fd8d72179fa6b755f36e2ff0a
Teal Wolf 25 <teal@tw25.net>
2026-04-10 22:34:57 -0400

    fabric metadata stuff

--- src/main/java/dev/evokerking/elemancy/datagen/ElemancyDataGenerator.java
+++ src/main/java/dev/evokerking/elemancy/datagen/ElemancyDataGenerator.java
@@ -1,4 +1,4 @@
-package dev.evokerking.elemancy;
+package dev.evokerking.elemancy.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
