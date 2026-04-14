9e0ab174e412070a5dacf8575202be2173571301
evokerking4 <evokerking2@gmail.com>
2026-04-14 13:07:25 +0000

    Changes.

---
+++ src/main/java/dev/evokerking/elemancy/items/EleminiteIngot.java
@@ -0,0 +1,13 @@
+package dev.evokerking.elemancy.items;
+
+import net.minecraft.world.item.Item;
+
+public class EleminiteIngot extends Item {
+    public static final String id = "eleminite";
+
+    public EleminiteIngot(Properties properties) {
+        super(properties);
+        //TODO Auto-generated constructor stub
+    }
+
+}
